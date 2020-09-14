faf0a4b6991989f93e456dbea966256a2fb186aa
Paul_Robinson <pr6784@gmail.com>
2020-09-14 15:10:35 +1000

    fixed open_shortcuts args and documented

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+"""
+small project for experimenting with speech recognition by opening shortcuts
+using pynput by moses-palmer and speechrecognition by Uberi
+"""
+
 import pynput
+import speech_recognition as sr
 
 from voice.voice_shortcuts import Voice
 
-keyboard = pynput.keyboard
-
 def main():
-    
-    while(True):
+    """
+    function for calling Voice.open_shortcut if / is pressed
+    """
+    while True:
         set_pressed = key_listen()
         if '/' in set_pressed:
-            v = Voice(keyboard)
-            v.open_shortcut()
+            voice = Voice(pynput.keyboard, sr)
+            voice.open_shortcut()
 
 def key_listen():
+    """
+    function for detecting key presses and returning them in a set
+    """
 
     pressed = set()
 
@@ -22,8 +31,8 @@ def key_listen():
 
     with pynput.keyboard.Listener(on_release=on_release) as listener:
         listener.join()
-    
-    return(pressed)
+
+    return pressed
 
 if __name__ == "__main__":
     print('Voice shortcuts ready press / to listen')
--- voice/voice_shortcuts.py
+++ voice/voice_shortcuts.py
@@ -1,33 +1,46 @@
-import speech_recognition as sr
+"""
+Contains class for activating voice shortcuts
+"""
 
 class Voice():
     """
-
+    Class for activating keyboard shortcuts using the python speech recognition library
+    variables:
+    self.keyboard: pynput keyboard class
+    self.recongiser: speechrecognition Recognizer
+    self.mic: speechrecognition Microphone
+    self.controller: pynput keyboard controller
+    self.shortcuts_dict: dictionary containing the shortcuts able to be activated stored as
+    'string name of shortcut to be spoken':((tuple of modifer keys), key press to activate shortcut)
     """
-    def __init__(self, keyboard):
+    def __init__(self, keyboard, sr):
         self.keyboard = keyboard
+        self.recogniser = sr.Recognizer()
+        self.mic = sr.Microphone()
         self.controller = self.keyboard.Controller()
         self.shortcuts_dict = {
             'task manager':((self.keyboard.Key.ctrl_l, self.keyboard.Key.shift_l), self.keyboard.Key.esc)
         }
 
     def listen(self):
+        """
+        Method for listening for microphone input using speechrecognition and returning the resulting string
+        """
         print("listening ....")
-        r = sr.Recognizer()
-        mic = sr.Microphone()
-        with mic as source:
-            audio = r.listen(source)
-        response = r.recognize_google(audio)
-        print(response)
-        return(response)
-    
-    def open_shortcut(self):
-        response = self.listen()
+        with self.mic as source:
+            audio = self.recogniser.listen(source)
+        response = self.recogniser.recognize_google(audio)
         print(f'detected {response}')
+        return response
+
+    def open_shortcut(self):
+        """
+        Method for opening shortcuts in shortcuts_dict using string from listen() method and pynput
+        """
+        response = self.listen()
         if response in self.shortcuts_dict:
             print(f'opening {response}')
-            #TODO make number of modifers varable
-            with self.controller.pressed(self.shortcuts_dict[response][0][0], self.shortcuts_dict[response][0][1]):
+            with self.controller.pressed(*self.shortcuts_dict[response][0]):
                 self.controller.press(self.shortcuts_dict[response][1])
         else:
             print('shortcut not found')
